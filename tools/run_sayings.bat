51a210db5d39ea5e594c9e51405a196d3498e9e7
anderseline <aps222@lehigh.edu>
2022-05-16 08:56:45 -0400

    change path

--- tools/run_sayings.bat
+++ tools/run_sayings.bat
@@ -1,3 +1,3 @@
 cd /d %~dp0
-"C:\Users\ander\AppData\Local\Programs\Python\Python39\python.exe" "..\main\GetSaying.py"
+"C:\Users\ander\AppData\Local\Programs\Python\Python39\python.exe" "..\main\HillsideProverbs.py"
 exit
